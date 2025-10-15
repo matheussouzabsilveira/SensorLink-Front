a29d08accd46d0e92af315b7c2d78e4f9da7ce50
MATHEUS DE SOUZA BERTO DA SILVEIRA <matheus_s_silveira@estudante.sesisenai.org.br>
2025-10-15 11:23:51 -0300

    cadastro update

--- Pages/Cadastro/Styles.js
+++ Pages/Cadastro/Styles.js
@@ -42,7 +42,7 @@ const styles = StyleSheet.create({
     alignItems: 'flex-end',
   },
   loginText: {
-    color: 'blue',
+    color: 'white',
     textDecorationStyle: 'solid',
     textDecorationColor: "blue",
     textDecorationLine: "underline",
